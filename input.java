01792b14b4dafb2daa957a951c0cbf7bc79dc8cf
vishesh1708 <svishesh1708@gmail.com>
2025-09-06 13:31:57 +0530

    first commit

---
+++ input.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+
+public class input {
+    public static void main(String[] args) {
+        
+    Scanner sc = new Scanner(System.in);
+    String name = sc.next();
+    System.out.println("My name is " + name);
+
+    sc.close();
+    }
+}
---
+++ output.java
@@ -0,0 +1,6 @@
+public class output {
+    public static void main(String[] args) {
+        System.out.println("My nam is vishesh sharma .");
+    }
+    
+}
