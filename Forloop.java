0b4a83ce4185d9bf610c8d4161847d8f11e004c1
vishesh1708 <svishesh1708@gmail.com>
2025-09-06 14:07:42 +0530

    loops

---
+++ Forloop.java
@@ -0,0 +1,14 @@
+public class Forloop {
+    public static void main(String[] args){
+        // for(int i=1;i<11;i++){
+        //     System.out.print(i+" ");
+
+         int i=1;
+        while(i<5){
+            System.out.print(i+" ");
+            i++;
+        }
+        }
+    }
+    
+
---
+++ naturalsum.java
@@ -0,0 +1,10 @@
+public class naturalsum { 
+    public static void main(String[] args){
+        int n=10;
+        int sum=0;
+        for(int i=1;i<=n;i++){
+            sum=sum+i;
+        }
+        System.out.println("The sum is "+sum);
+    }
+}
